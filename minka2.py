1e0144da9c9a532c5052d03b38030d4281244b92
Km1zZzoU <zhovnir.ea@gmail.com>
2023-10-14 22:56:27 +0700

    Fix minka2 and minka3

--- minka2.py
+++ minka2.py
@@ -1,11 +1,9 @@
 def MyZip(arr1, arr2):
     arr = []
     for i in range (min(len(arr1), len(arr2))):
-        arr.append([arr1[i], arr2[i]])
+        arr.append((arr1[i], arr2[i]))
     return(arr)
 
 arr1 = [1, 2, 3, 4, 5]
 arr2 = ['a', 'b', 'c', 'd']
 print(MyZip(arr1, arr2))
-                   
-        
--- minka3.py
+++ minka3.py
@@ -1,14 +1,25 @@
 string = input()
 mtrx = []
 mtrxSTR = []
-for i in range(len(string.split())):
+
+for i in range(len(string.split('|'))):
+    print(string.split('|'))
+    for j in range(len(string.split('|')[i].split())):
+        print(string.split('|')[i].split())
+        mtrxSTR.append(float(string.split('|')[i].split()[j]))
+        print(mtrxSTR)
+    mtrx.append(mtrxSTR)
+    print(mtrx)
+    mtrxSTR = []
+
+'''for i in range(len(string.split())):
     if string.split()[i]!='|':
         mtrxSTR.append(float(string.split()[i]))
     else:
         mtrx.append(mtrxSTR)
-        mtrxSTR = []
-mtrx.append(mtrxSTR)
-indexs = input()
+        mtrxSTR = []'''
+
+indexs = '0 1'
+print(mtrx)
 print(mtrx[int(indexs.split()[0])][int(indexs.split()[1])])
-    
 
